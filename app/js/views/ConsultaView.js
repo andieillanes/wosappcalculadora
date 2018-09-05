1eff52f21270201c3066bf3b409c01e593e070c1
Rafael Nai <ramiresnai@gmail.com>
2018-09-05 11:41:39 -0300

    Files compiled by Typescript

---
+++ app/js/views/ConsultaView.js
@@ -0,0 +1,57 @@
+System.register(["./View"], function (exports_1, context_1) {
+    "use strict";
+    var View_1, ConsultaView;
+    var __moduleName = context_1 && context_1.id;
+    return {
+        setters: [
+            function (View_1_1) {
+                View_1 = View_1_1;
+            }
+        ],
+        execute: function () {
+            ConsultaView = class ConsultaView extends View_1.View {
+                constructor(seletor) {
+                    super(seletor);
+                }
+                template(model) {
+                    return `
+            <div class="card my-2 col">
+                <div class="card-body text-center">
+                    <h5>Perder peso</h5>
+                    <p class="card-text">
+                        <span class="display-4 font-weight-bold">
+                            ${(model) ? parseFloat(model.calorias) - 500 : 0}
+                        </span>
+                        calorias
+                    </p>    
+                </div>
+            </div> <!-- /card -->
+
+            <div class="card m-2 col">
+                <div class="card-body text-center">
+                    <h5>Manter peso</h5>
+                    <p class="card-text">
+                        <span class="display-4 font-weight-bold">
+                            ${(model) ? parseFloat(model.calorias) : 0}
+                        </span>
+                        calorias
+                    </p>    
+                </div>
+            </div> <!-- /card -->
+
+            <div class="card my-2 col">
+                <div class="card-body text-center">
+                    <h5>Ganhar peso</h5>
+                    <span class="display-4 font-weight-bold">
+                        ${(model) ? parseFloat(model.calorias) + 350 : 0}
+                    </span>
+                    calorias  
+                </div>
+            </div> <!-- /card -->
+        `;
+                }
+            };
+            exports_1("ConsultaView", ConsultaView);
+        }
+    };
+});
